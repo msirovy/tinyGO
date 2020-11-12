6bdb6220a8f580acff47aad7aa44e4fe637add36
Marek Sirovy <msirovy@gmail.com>
2020-11-12 17:07:06 +0100

    improvements

--- ar-blink/blink_ext.go
+++ ar-blink/blink_ext.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	led := machine.D12
-	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	led1 := machine.D12
+	led1.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	led2 := machine.D13
+	led2.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	// Define parameters
 	var min, max int16 = 50, 500
@@ -15,16 +18,20 @@ func main() {
 
 	for {
 		for i := min; i < max; i=i+step {
-			led.High()
+			led1.High()
+			led2.Low()
 			time.Sleep(time.Millisecond * time.Duration(i))
-			led.Low()
+			led1.Low()
+			led2.High()
 			time.Sleep(time.Millisecond * time.Duration(i))
 		}
 
 		for i := max; i > min; i=i-step {
-			led.High()
+			led1.High()
+			led2.Low()
 			time.Sleep(time.Millisecond * time.Duration(i))
-			led.Low()
+			led1.Low()
+			led1.High()
 			time.Sleep(time.Millisecond * time.Duration(i))
 		}
 
